fe74d3217480d28d428bef4d71d26c11e7279df1
NiravGolyalla <ng2411@nyu.edu>
2022-10-28 11:41:44 -0400

    Destroy bullets on walls

--- Assets/Code/OverworldLerp.cs
+++ Assets/Code/OverworldLerp.cs
@@ -14,31 +14,17 @@ public class OverworldLerp : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.S)){MoveDown();}
            
     }
-    IEnumerator Moveup(){
-        float t = 0;
-        while(t < .8f){
-            if(currentIndex < 4){
-                transform.position = Vector3.Lerp(transform.position, platforms[currentIndex+1].transform.position,t*moveSpeed);
-                t += Time.deltaTime;
-                yield return null;
-            } else{
-                t = 1f;
-                yield return null;
-            }
+    void Moveup(){
+        if(currentIndex < 4 && PublicVars.l_status[currentIndex]){
+            GameObject destination = platforms[currentIndex++];
+            gameObject.transform.position = destination.transform.position;
         }
     }
 
-    IEnumerator MoveDown(){
-        float t = 0;
-        while(t < .8f){
-            if(currentIndex > 0){
-                transform.position = Vector3.Lerp(transform.position, platforms[currentIndex-1].transform.position,t*moveSpeed);
-                t += Time.deltaTime;
-                yield return null;
-            } else{
-                t = 1f;
-                yield return null;
-            }
+    void MoveDown(){
+        if(currentIndex > 0){
+            GameObject destination = platforms[currentIndex--];
+            gameObject.transform.position = destination.transform.position;
         }
     }
 
