a05aca671d7adc0077600e144b2bce01cb22c6f7
NiravGolyalla <ng2411@nyu.edu>
2022-10-29 00:08:27 -0400

    Checks

--- Assets/Code/Victory.cs
+++ Assets/Code/Victory.cs
@@ -3,23 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Victory : MonoBehaviour
-{
+public class Victory : MonoBehaviour{
     public GameObject win;
     public GameObject neutral;
     public GameObject defeat;
     public GameObject monster;
+    public static int killed;
 
     void Start(){
-        int killed = PublicVars.killed;
-        if( killed< 30){
+        if(killed < 30){
             defeat.SetActive(true);
-        } else if(killed < 60){
-            neutral.SetActive(true);
-        } else if(killed < 100){
-            win.SetActive(true);
         } else{
-            monster.SetActive(true);
+            if(killed < 60){
+                neutral.SetActive(true);
+                } else{
+                    if(killed < 90){
+                        win.SetActive(true);
+                        } else{
+                            monster.SetActive(true);
+                            }
+                        }
         }
     }
 }
+
